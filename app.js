0d66031564e45d1e3d6361d994a32bc16d29086a
scudett0 <glanternlight@gmail.com>
2024-03-19 13:27:49 -0400

    git commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const board = document.querySelector("#board")
+const playerPick = document.querySelector("#player")
+const info = document.querySelector("#info")
+const width = 8
+const startingPiece = {
+    
+}
